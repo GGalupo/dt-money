bde7534c4f90de296cd7a03e65e20b81e0ab2b30
Gustavo Galupo <ggalupo2@hotmail.com>
2021-05-10 23:39:00 -0300

    NewTransactionModal component and code refactoring.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import { useState } from 'react'
+
 import { GlobalStyle } from './styles/global'
 
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal'
+
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+    
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
